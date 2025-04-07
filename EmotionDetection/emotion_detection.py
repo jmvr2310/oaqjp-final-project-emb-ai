31fb9fcdc5083fbef48c98302a9ccbb653dbf338
jose <jmvr.2310@gmail.com>
2025-04-06 22:22:25 -0400

    third commit

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -10,9 +10,9 @@ def emotion_detector(text_to_analyse):
     formatted_response = json.loads(response.text)
 
     predictions = formatted_response.get("emotionPredictions", [])
-    
+
     emotions = predictions[0].get("emotion", {})
-    
+   
     anger_score = emotions.get("anger", 0)
     disgust_score = emotions.get("disgust", 0)
     fear_score = emotions.get("fear", 0)
@@ -21,10 +21,18 @@ def emotion_detector(text_to_analyse):
 
     emotion_scores = [anger_score, disgust_score, fear_score, joy_score, sadness_score]
     emotion_keys = ["anger", "disgust", "fear", "joy", "sadness"]
-
+  
     dominant_emotion_index = emotion_scores.index(max(emotion_scores))
     dominant_emotion_key = emotion_keys[dominant_emotion_index]
 
+    if response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dominant_emotion_key = None
+    
     result = {
         'anger': anger_score,
         'disgust': disgust_score,
